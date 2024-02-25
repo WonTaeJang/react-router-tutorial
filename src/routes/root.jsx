40adbbdd63def40d5735a00e548604d1ccab1760
WonTaeJang <jwt0212@gmail.com>
2024-02-25 16:57:39 +0900

    global pending ui - 페이지 이동시 로딩이 걸린걸 표시 및 추가적인 요청을 방지할 수 있음

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,16 @@
-import { Link, Outlet, useLoaderData, Form } from "react-router-dom"
+import { 
+  Outlet,
+  NavLink,
+  useLoaderData, 
+  Form, 
+  redirect,
+  useNavigation,
+} from "react-router-dom"
 import { getContacts, createContact } from '../contacts.js'
 
 export async function action() {
   const contact = await createContact()
-  return { contact }
+  return redirect(`/contacts/${contact.id}/edit`)
 }
 
 export async function loader() {
@@ -15,6 +22,7 @@ export async function loader() {
 
 export default function Root() {
     const { contacts } = useLoaderData()
+    const navigation = useNavigation()
     console.log(contacts)
 
   return (
@@ -49,7 +57,14 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active" : isPending
+                        ? "pending": ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -58,7 +73,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -69,7 +84,12 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div 
+        id="detail"
+        className={
+          navigation.state === 'loading' ? 'loading' : ''
+        }
+      >
         <Outlet />
       </div>
     </>
