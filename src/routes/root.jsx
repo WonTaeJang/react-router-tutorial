0378a273bd4e7309df74e9bdc49e180b7124a4bd
WonTaeJang <jwt0212@gmail.com>
2024-02-25 19:39:10 +0900

    managing the history stack

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -50,7 +50,10 @@ export default function Root() {
               defaultValue={q}
               className={searching ? "loading" : ""}
               onChange={(event) => {
-                submit(event.currentTarget.form)
+                const isFirstSearch = q === null
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch
+                })
               }}
             />
             <div
